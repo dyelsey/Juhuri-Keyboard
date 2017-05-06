caee71313e5192ae984817b4eb706780c592ac54
David Yelsey <dyelsey@users.noreply.github.com>
2017-05-06 02:04:45 -0400

    Update add_rule.py

--- Linux jdt keyboard/add_rule.py
+++ Linux jdt keyboard/add_rule.py
@@ -1,34 +1,28 @@
-import sys
+#David Yelsey
+#Adds keyboard in evdev.xml right before </layoutList>
+
 l = []
-for i in range(23):
+for i in range(8):
 	l.append('')
 l[0] = "     <layout>\n"
 l[1] = "      <configItem>\n"
-l[2] = "        <name>jdt-lat</name>\n"
-l[3] = "        <shortDescription>jdt-lat</shortDescription>\n"
-l[4] = "        <description>Judeo-Tat (Latin)</description>\n"
-l[5] = "        <languageList><iso639Id>jdt</iso639Id><iso639Id>jdt-lat</iso639Id></languageList>\n"
+l[2] = "        <name>jdt-cyr</name>\n"
+l[3] = "        <shortDescription>jdt-cyr</shortDescription>\n"
+l[4] = "        <description>Judeo-Tat (Cyrillic)</description>\n"
+l[5] = "        <languageList><iso639Id>jdt</iso639Id><iso639Id>jdt-cyr</iso639Id></languageList>\n"
 l[6] = "      </configItem>\n"
-l[6] = "    </layout>\n"
-l[7] = "     <layout>\n"
-l[8] = "      <configItem>\n"
-l[9] = "        <name>jdt-cyr</name>\n"
-l[10] = "        <shortDescription>jdt-cyr</shortDescription>\n"
-l[11] = "        <description>Judeo-Tat (Cyrillic)</description>\n"
-l[12] = "        <languageList><iso639Id>jdt</iso639Id><iso639Id>jdt-cyr</iso639Id></languageList>\n"
-l[13] = "      </configItem>\n"
-l[14] = "    </layout>\n"
+l[7] = "    </layout>\n"
 
+to_add = ''
+for line in l:
+	to_add += line
 
-with open('evdex.xml', 'r+') as f:
-	read_data = f.read()
-	data = read_data.split('\n')
-	for line in range(len(data)):
-		#print line
-		if '</layoutList>'in data[line]:
+buf = []
+with open("evdev.xml", "r") as in_file:
+    buf = in_file.readlines()
 
-			line = sys.getsizeof(data[:line])
-			f.seek(line)
-			for i in l:
-				f.write(i)
-
+with open("evdev.xml", "w") as out_file:
+    for line in buf:
+        if "</layoutList>" in line:
+            line = to_add + line
+        out_file.write(line)
